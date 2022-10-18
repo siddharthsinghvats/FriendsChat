2ed47fb5671729bec8c26eddeba541012f259e45
Siddharth <siddharth1singh1@gmail.com>
2022-10-18 12:33:15 +0530

    fix input at bottom

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import {ChatEngine} from 'react-chat-engine';
 import ChatFeed from './components/ChatFeed';
-import './app.css';
+import './App.css';
 import LoginForm from './components/LoginForm';
 
 const PID = process.env.REACT_APP_PID;
@@ -8,7 +8,7 @@ const App = ()=>{
     if(!localStorage.getItem('username')) return <LoginForm/>
     return(
         <ChatEngine height="100vh" 
-        projectID={PID}
+        projectID="d3f38313-a7fb-4faa-9b60-16225c722b81"
         userName={localStorage.getItem('username')}
         userSecret={localStorage.getItem('password')}
         renderChatFeed ={(chatAppProps)=><ChatFeed {...chatAppProps}/>}
